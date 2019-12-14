9a1f4f33d28ad1ccff9cc7a09fe7847aa1471ab1
Abu Hajar Hakim Mohammed Ismal Hasam Allah Iraq Shariah Mecca Pakistan <49108786+Abu-Hajar-Hakim-Mohammed-Ismal-Hasam@users.noreply.github.com>
2019-12-13 23:23:49 -0800

    Update VirtualTablePractice.cpp

--- VirtualTablePractice.cpp
+++ VirtualTablePractice.cpp
@@ -8,11 +8,10 @@
 class cX {
 public:
 	virtual void function1() { std::cout << "Function1 called" << std::endl; }
-	int int1 = 5;
+	int int1 = 5; // Does not include this variable as it is not stored in the vtable (not "virtual")
 
 	
 };
-
 
 
 class derivedcX : public cX
@@ -24,13 +23,9 @@ public:
 };
 
 
+derivedcX* derivedClass = new derivedcX;
 
-
-
-derivedcX* y = new derivedcX;
-
-//::std::uint32_t* AddressOfClass = reinterpret_cast<::std::uint32_t*>(x);
-::std::uint32_t* AddressOfDerivedClass = reinterpret_cast<::std::uint32_t*>(y);
+::std::uint32_t* AddressOfDerivedClass = reinterpret_cast<::std::uint32_t*>(derivedClass);
 ::std::uint32_t* AddressOfFirstFunction = reinterpret_cast<::std::uint32_t*>(*AddressOfDerivedClass + 0x0);
 int(*ptrToClassFunc1)() = reinterpret_cast<int(*)()>(*AddressOfFirstFunction);
 ::std::uint32_t* AddressOfDerivedFunction = reinterpret_cast<::std::uint32_t*>(*AddressOfDerivedClass + 0x4);
@@ -54,19 +49,8 @@ int main()
 	{
 		if (GetAsyncKeyState(VK_RSHIFT))
 		{
-			delete y;
+			delete derivedClass;
 			return 0;
 		}
 	}
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
